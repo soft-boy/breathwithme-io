ab54e16dbd5ac51b4327ecdb381f133a885b2eef
Eric Wang <ewang1997@gmail.com>
2022-12-17 14:41:12 -0800

    add social meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,16 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>4-7-8 Breathing | BreathWithMe.io</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@BreathFlowHW" />
+        <meta name="twitter:title" content="4-7-8 Breathing | BreathWithMe.io" />
+        <meta name="twitter:description" content="🍵 Let's take a few deep breaths together." />
+        <meta name="twitter:image" content="https://breathwithme.io/preview.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="4-7-8 Breathing | BreathWithMe.io" />
+        <meta property="og:description" content="🍵 Let's take a few deep breaths together." />
+        <meta property="og:url" content="https://breathwithme.io/" />
+        <meta property="og:image" content="https://breathwithme.io/preview.png" />
       </Head>
       <Component {...pageProps} />
     </main>
