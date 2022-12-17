1e3f321063e2b40c6fbc93c336f3a11ba18be23b
Eric Wang <ewang1997@gmail.com>
2022-12-17 15:19:24 -0800

    fix twitter card

--- pages/_app.js
+++ pages/_app.js
@@ -11,11 +11,11 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>4-7-8 Breathing | BreathWithMe.io</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@BreathFlowHW" />
-        <meta name="twitter:title" content="4-7-8 Breathing | BreathWithMe.io" />
-        <meta name="twitter:description" content="🍵 Let's take a few deep breaths together." />
-        <meta name="twitter:image" content="https://breathwithme.io/preview.png" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:site" content="@BreathFlowHQ" />
+        <meta property="twitter:title" content="4-7-8 Breathing | BreathWithMe.io" />
+        <meta property="twitter:description" content="🍵 Let's take a few deep breaths together." />
+        <meta property="twitter:image" content="https://breathwithme.io/preview.png" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="4-7-8 Breathing | BreathWithMe.io" />
         <meta property="og:description" content="🍵 Let's take a few deep breaths together." />
